1c052deee1c577a4d0199a3b0b414908b63123be
kaige.yuan <kaige.yuan@ly.com>
2021-09-03 14:58:01 +0800

    新增服务端入口类

---
+++ src/GreetingServer.java
@@ -0,0 +1,41 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GreetingServer {
+
+    public static void main(String[] args) {
+
+        int port = 7000;
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(port);
+            Socket socket = serverSocket.accept();
+
+            DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+            System.out.println(dataInputStream.readUTF());
+
+//            System.out.println(String.format("欢迎来自%s的%s", dataInputStream.readUTF()));
+            dataOutputStream.writeChars("你好");
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
